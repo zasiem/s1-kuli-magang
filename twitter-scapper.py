c845462ce13df8ba4db3ab99357fb430f071a837
Erza Putra Albasori <erzaputra9110@gmail.com>
2020-12-17 00:06:46 +0700

    add: looping in scraper

--- twitter-scapper.py
+++ twitter-scapper.py
@@ -6,21 +6,41 @@ auth.set_access_token('key', 'key')
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-text_query = 'lang:id #StartUpEP1'
-count = 100
-try:
- # Creation of query method using parameters
- tweets = tweepy.Cursor(api.search,q=text_query).items(count)
- 
- # Pulling information from tweets iterable object
- tweets_list = [[tweet.created_at, tweet.id, tweet.text] for tweet in tweets]
- 
- # Creation of dataframe from tweets list
- # Add or remove columns as you remove tweet information
- tweets_df = pd.DataFrame(tweets_list)
- print(tweets_df)
+text_queries = [
+    'lang:id #StartUpEP1',
+    'lang:id #StartUpEP2',
+    'lang:id #StartUpEP3',
+    'lang:id #StartUpEP4',
+    'lang:id #StartUpEP5',
+    'lang:id #StartUpEP6',
+    'lang:id #StartUpEP7',
+    'lang:id #StartUpEP8',
+    'lang:id #StartUpEP9',
+    'lang:id #StartUpEP10',
+    'lang:id #StartUpEP11',
+    'lang:id #StartUpEP12',
+    'lang:id #StartUpEP13',
+    'lang:id #StartUpEP14',
+    'lang:id #StartUpEP15',
+    'lang:id #StartUpEP16',
+]
 
- tweets_df.to_csv("file1.csv")
- 
-except BaseException as e:
-    print('failed on_status,',str(e))
+count = 5
+
+for index, text_query in enumerate(text_queries) :
+    try:
+        # Creation of query method using parameters
+        tweets = tweepy.Cursor(api.search,q=text_query).items(count)
+        
+        # Pulling information from tweets iterable object
+        tweets_list = [[tweet.created_at, tweet.id, tweet.text] for tweet in tweets]
+        
+        # Creation of dataframe from tweets list
+        # Add or remove columns as you remove tweet information
+        tweets_df = pd.DataFrame(tweets_list)
+        print(tweets_df)
+
+        tweets_df.to_csv("File Startup {}.csv".format(index+1))
+        
+    except BaseException as e:
+        print('failed on_status,',str(e))
